5d8ea72ebeae082d2356c5b9ebee29fb71d5db5c
hopsm.dev <hopsm.dev@gmail.com>
2016-01-21 19:57:53 +0100

    add login page

--- mtw/mtw.py
+++ mtw/mtw.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/index")
 def index():
     return render_template('index.html')
 
@@ -11,3 +16,7 @@ def index():
 @app.route('/a', methods=['GET', 'POST'])
 def movie():
     return render_template('index.html', url=request.form['search'])
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
