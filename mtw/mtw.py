5aa31e9b2b69f454ac4dd72c80f7eff2dca2ac14
hopsm.dev <hopsm.dev@gmail.com>
2016-01-21 20:34:11 +0100

    add register page

--- mtw/mtw.py
+++ mtw/mtw.py
@@ -8,6 +8,11 @@ def login():
     return render_template('login.html')
 
 
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+
 @app.route("/index")
 def index():
     return render_template('index.html')
