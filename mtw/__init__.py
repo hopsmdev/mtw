2647661b194652de813c4827243a20f297d0f37e
hopsm.dev <hopsm.dev@gmail.com>
2016-01-27 22:34:43 +0100

    add blueprints to create_app func

--- mtw/__init__.py
+++ mtw/__init__.py
@@ -7,6 +7,8 @@ from config import config
 
 mongo = PyMongo()
 login_manager = LoginManager()
+login_manager.session_protection = 'basic'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -15,5 +17,11 @@ def create_app(config_name):
 
     mongo.init_app(app)
     login_manager.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
